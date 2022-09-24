00139ac16a12900e66b9fbf8ecdbeb2e0e5da4dd
Salvo Russo <salvatore.russo01@leonardocompany.com>
2022-09-24 11:54:18 +0200

    Refactor totem and ticket

--- contracts/roadpasschain.sol
+++ contracts/roadpasschain.sol
@@ -8,48 +8,103 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
+contract RoadPassChain {
+    mapping(address => string) _totems;
+
+    function registerTotem(address totem, string memory location) internal {
+        _totems[totem] = location;
+    }
+
+    function getLocation(address gate) public view returns (string memory) {
+        return _totems[gate];
+    }
+}
+
 /** 
- * @title RoadPassChain
- * @dev Implements payment process of tolls and parking in smart cities
+ * @title RoadPassTotem
+ * @dev Uniquely associated with the motorway columns, implements payment methods
  */
-contract RoadPassChain is ERC721Burnable, Ownable {
-    using Counters for Counters.Counter;
+contract RoadPassTotem is RoadPassChain {
+    address payable public _wallet; //Wallet of the motorway company
+    RoadPassTicket _ticket; //Contract of NFTs
+    //string _location;
+    uint256 _price;
+
+    constructor(address payable wallet, string memory location, uint256 price, address ticket) {
+        _wallet = wallet;
+        //_location = location;
+        _price = price;
+        _ticket = RoadPassTicket(ticket);
+        //Registriamo la colonnina
+        super.registerTotem(address(this), location);
+    }
+
+
+    function entrance(address to) public returns (uint256) {
+        console.log("MY address: ", to);
+        return _ticket.createTicket(address(this), to);
+    }
+
+    function exit(uint256 ticketId) external payable {
+        uint256 cost = calculateCost(_ticket.entranceGate(ticketId));
+        require(msg.value == cost, "Not enough ETH sent; check price!");
+        _ticket.burnTicket(address(this), ticketId);
+    }
+
+    function calculateCost(address entranceGate) public returns (uint256){
+        return 1;
+    }
+
+}
+
+/** 
+ * @title RoadPassTicket
+ * @dev Unique tickets that guarantee the right to transit
+ */
+contract RoadPassTicket is ERC721Burnable, Ownable, RoadPassChain {
+    address payable public wallet; //Wallet of the motorway company
+    using Counters for Counters.Counter; //Counter for NFTs
     Counters.Counter private _tokenIds;
-    address payable public wallet;
 
-    mapping (uint256 => string) private tokenIdToLocation;
+    mapping(uint256 => address) private _entranceGates;
+    mapping(uint256 => address) private _exitGates;
 
-    constructor(address payable _wallet) ERC721("RoadTicket", "NFT") {
+    constructor(address payable _wallet) ERC721("Ticket", "RoadPassChain") {
         wallet = _wallet;
     }
 
-    function entrance(address customer, string calldata gate) public onlyOwner returns (uint256)
-    {
+    function createTicket(address gate, address to) public returns (uint256) {   
+        console.log("Creating a new ticket...");
+        string memory location = super.getLocation(gate);
+        console.log("Creating ticket at ", location);   
+        require(bytes(location).length > 0, "Totem not exists!");
         _tokenIds.increment();
         uint256 ticketId = _tokenIds.current();
-        _mint(customer, ticketId);
-        tokenIdToLocation[ticketId] = gate;
+        _mint(to, ticketId);
+        _entranceGates[ticketId] = gate;
         return ticketId;
     }
 
-    function exit(uint256 ticketId, string calldata password) public payable
-    {
-        console.log("VALUE: ", msg.value);
-        console.log("Entrance Gate: ", getLocation(ticketId));
-        //Calcola costo
-        uint256 cost = calculateCost(getLocation(ticketId), password);
-        require(msg.value == cost, "Not enough ETH sent; check price!");
-        wallet.transfer(msg.value); 
-        burn(ticketId);
+    function burnTicket(address gate, uint256 ticketId) public {
+        console.log("Entrance Gate: ", entranceGate(ticketId));
+        console.log("Exit Gate: ", gate);
+        setExitGate(ticketId, gate);
+        burn(ticketId);        
     }
 
-    //this function gets a value from the map
-    //If a value was not set the function will return the default value of 0.
-    function getLocation(uint256 ticketId) public view returns (string memory) {
-        return tokenIdToLocation[ticketId];
+    function setEntranceGate(uint256 tokenId, address gate) private {
+        _entranceGates[tokenId] = gate;
     }
 
-    function calculateCost(string memory entrance, string memory password) private returns (uint256 cost) {
-        return 1;
+    function setExitGate(uint256 tokenId, address gate) private {
+         _exitGates[tokenId] = gate;
+    }
+
+    function entranceGate(uint256 tokenId) public view returns (address) {
+        return _entranceGates[tokenId];
+    }
+
+    function exitGate(uint256 tokenId) public view returns (address) {
+        return _exitGates[tokenId];
     }
 }
